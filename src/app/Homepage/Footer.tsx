79b8e26e7b601d38fa133c282b0c053c2c7ca844
Kono1889 <agyemangkono1889@gmail.com>
2024-07-25 11:34:22 +0000

    edited the footer, added Meal card, feedback section

--- src/app/Homepage/Footer.tsx
+++ src/app/Homepage/Footer.tsx
@@ -1,43 +1,47 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { FaFacebook } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 
 export const Footer = () => {
   return (
     <div className="mt-[9rem] p-4 bg-[#FEFDF5]">
       <Head>
-        <title>Breakfast To Breakfast</title>
+        <title >Breakfast To Breakfast</title>
         <meta name="description" content="Breakfast To Breakfast restaurant information" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mb-8">
-          <header className="bg-white p-4 rounded shadow col-span-4 md:col-span-1">
-            <Image src="/logo.png" alt="Breakfast To Breakfast Logo" width={50} height={50} />
-            <h1 className="ml-4 text-2xl font-bold">Breakfast To Breakfast</h1>
-          </header>
+        <header className=" p-4  col-span-4 md:col-span-1 flex items-start">
+        <Image src="/logo.png" alt="Breakfast To Breakfast Logo" width={50} height={50} />
+        <h1 className="ml-4 text-[1rem] font-bold mt-[1rem]">Breakfast To Breakfast</h1>
+      </header>
 
-          <section className="bg-white p-4 rounded shadow col-span-4 md:col-span-1">
-            <h2 className="text-xl font-semibold mb-4">Opening Hours</h2>
+
+          <section className=" p-4  col-span-4 md:col-span-1">
+            <h2 className="text-[1rem] font-semibold mb-4">Opening Hours</h2>
             <ul>
-              <li>Sunday: 08:00am - 6:00pm</li>
-              <li>Monday: 08:00am - 8:00pm</li>
-              <li>Tuesday: 08:00am - 8:00pm</li>
-              <li>Wednesday: 08:00am - 8:00pm</li>
-              <li>Thursday: 08:00am - 8:00pm</li>
-              <li>Friday: 08:00am - 8:00pm</li>
-              <li>Saturday: 08:00am - 6:00pm</li>
+              <li className='text-[0.9rem] mb-[1rem]'>Sunday: 08:00am - 6:00pm</li>
+              <li className='text-[0.9rem] mb-[1rem]'>Monday: 08:00am - 8:00pm</li>
+              <li className='text-[0.9rem] mb-[1rem]'>Tuesday: 08:00am - 8:00pm</li>
+              <li className='text-[0.9rem] mb-[1rem]'>Wednesday: 08:00am - 8:00pm</li>
+              <li className='text-[0.9rem] mb-[1rem]'>Thursday: 08:00am - 8:00pm</li>
+              <li className='text-[0.9rem] mb-[1rem]'>Friday: 08:00am - 8:00pm</li>
+              <li className='text-[0.9rem] mb-[1rem]'>Saturday: 08:00am - 6:00pm</li>
             </ul>
           </section>
 
-          <section className="bg-white p-4 rounded shadow col-span-4 md:col-span-1">
+          <section className=" p-4  col-span-4 md:col-span-1">
             <h2 className="text-xl font-semibold mb-4">Contact Us</h2>
-            <p>Address: Osu 6th St, Accra</p>
-            <p>Phone: 020 880 0008</p>
-            <p>Email: b2baccraght@gmail.com</p>
+            <p className='mb-[0.5rem]'>Address: Osu 6th St, Accra</p>
+            <p className='mb-[0.5rem]'>Phone: 020 880 0008</p>
+            <p className='mb-[0.5rem]'>Email: b2baccraght@gmail.com</p>
           </section>
 
-          <section className="bg-white p-4 rounded shadow col-span-4 md:col-span-1">
+          <section className=" p-4  col-span-4 md:col-span-1">
             <h2 className="text-xl font-semibold mb-4">Feedback</h2>
             <p className="mb-4">Let us know what is on your mind. Send us your feedback</p>
             <form>
@@ -50,9 +54,9 @@ export const Footer = () => {
 
         <footer className="mt-8 text-center">
           <div className="flex justify-center space-x-4 mb-4">
-            {/* <a href="#"><Image src="/instagram.png" alt="Instagram" width={24} height={24} /></a>
+            <a href="#"><Image src="/instagram.png" alt="Instagram" width={24} height={24} /></a>
             <a href="#"><Image src="/facebook.png" alt="Facebook" width={24} height={24} /></a>
-            <a href="#"><Image src="/twitter.png" alt="Twitter" width={24} height={24} /></a> */}
+            <a href="#"><Image src="/twitter.png" alt="Twitter" width={24} height={24} /></a>
           </div>
           <p>© 2018 - 2024</p>
         </footer>
--- src/app/Homepage/SecondSec.tsx
+++ src/app/Homepage/SecondSec.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Minicard from './Minicard';
 import Image from 'next/image';
+import MealCard from './MealCard';
 import { CiTimer } from "react-icons/ci";
 import { IoLocation } from "react-icons/io5";
+import { BiSolidPhoneCall } from "react-icons/bi";
+import { GiCampCookingPot } from "react-icons/gi";
+import { BsCart3 } from "react-icons/bs";
+import { PiBicycle } from "react-icons/pi";
+
+
+
 
 const SecondSec = () => {
   return (
@@ -10,6 +18,8 @@ const SecondSec = () => {
         <div className='ml-[15rem] mt-[4rem] flex flex-row gap-[10rem]'>
             <Minicard icon={<IoLocation className="h-[3em] w-[3em]"/>} text="Osu Oxford Street 6th ST, Accra"/>
             <Minicard icon={<CiTimer className="h-[3em] w-[3em]"/>} text="Open 24/7"/>
+            <Minicard icon={<BiSolidPhoneCall className="h-[3em] w-[3em]"/>} text="Open 24/7"/>
+
         </div>
         <div className='flex flex-row'>
             <div className='flex flex-col gap-[0.5rem] mt-[1rem] w-[70%]'>
@@ -21,17 +31,61 @@ const SecondSec = () => {
                 </div>
             </div>
 
-            <div className='w-[30%]'>
+            <div className='w-[30%] mt-[2rem]'>
                 <h1 className='text-[3rem] font-semibold'>About <span className='text-sky-500'>us</span></h1>
                 <p className='text-wrap'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 
                 <a href="#" className="inline-block mt-[3rem] px-4 py-[0.7rem] text-white text-[13px] bg-sky-500 rounded-[8px] hover:bg-slate-600">
-                    See more
+                    See more 
                 </a>
             </div>
+
         </div>
+        <div className=' mt-[2rem]'>
+            <h1 className=' text-center mt-[2rem] font-bold text-[2rem]'>How it <span className='text-sky-500'>works</span></h1>
+            <div className='ml-[15rem] mt-[4rem] flex flex-row gap-[10rem]'>
+            <MealCard heading='Pick out your meals' text='Scroll through your menu, and select your preferred meal.' image={<GiCampCookingPot/>}/>
+            <MealCard heading='Place your order' text='Open the cart, Check your Order, then checkout.' image={<BsCart3/>}/>
+            <MealCard heading='Get quick delivery.' text='Your meal would be delivered to your doorstep as soon as possible.' image={<PiBicycle/>}/>
+            </div>
+        </div>
+        <div className='mt-[4rem] flex justify-center'>
+            <Image src='/exploreimg.png' alt='' width={200} height={200}/>
+        </div>
+
+        <div className='mt-[4rem] flex justify-center'>
+            <p className='text-bold text-[2rem] font-bold '>Explore <span className='border-b-[3.5px] border-current'>our</span> best <span className='text-sky-500'>Menu</span></p>
+        </div >
+        <div className='flex justify-center mx-[30%] my-[0.5%]'>
+            <p className='text-center '>
+                Indulge in our mouth-watering selection of best-selling dishes, crafted with the finest ingredients and utmost care. From savory appetizers to decadent desserts, our menu offers something for every palate.
+            </p>
+        </div>
+        <div className="text-center">
+            <a href="#" className="inline-block px-[6rem] py-[0.7rem] text-white text-[13px] bg-slate-500 rounded-[8px] hover:bg-slate-600">
+                View Menu
+            </a>
+        </div>
+
+        <div className="text-center my-[3rem]">
+           <p className='font-bold text-[1.7rem]'>Customer <span className='text-sky-500'>Review</span></p>
+        </div>
+        <div className='border border-gray-300 p-4 mx-[35%] rounded-md'>
+            <p className='text-center text-[0.8rem] mb-4 font-bold'>Have any feedback?<br/>Tell us.</p>
+            <div className='relative flex justify-center'>
+                <input
+                    type="text"
+                    placeholder="Yourname@gmail.com"
+                    className="border border-gray-300 rounded-md px-6 py-2 w-80 pr-20"
+                />
+                <button className="absolute right-2 top-1/2 transform -translate-y-1/2 -translate-x-14 bg-gray-500 text-white text-[0.6rem] px-5 py-[0.5rem] rounded-md hover:bg-gray-600">Send</button>
+            </div>
+        </div>
+
+
+
     </div>
   )
 }
 
-export default SecondSec
+export default SecondSec;
