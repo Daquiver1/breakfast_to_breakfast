815ccc941c2658d2479616874bb370540ffbfdad
Daquiver1 <Cabrokwa11@gmail.com>
2024-09-13 05:55:55 +0000

    fix#4

--- src/app/contact/contactComponent.tsx
+++ src/app/contact/contactComponent.tsx
@@ -60,7 +60,7 @@ const ContactUsComponent = () => {
               />
               <textarea
                 placeholder="Your message"
-                rows="4"
+                rows={4}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
               ></textarea>
               <button
